d4fe5334b59adc4a436dee90190cf30d99f32c67
Kishore <kishore21092003@gmail.com>
2025-09-09 15:01:58 +0530

    update

--- Database/Schema/user.js
+++ Database/Schema/user.js
@@ -1,38 +1,38 @@
 const mongoose = require('mongoose');
 
-const UserSchema = mongoose.Schema({
-    UserName:{
-        type:String,
-        require:true
+const UserSchema = new mongoose.Schema({
+    UserName: {
+        type: String,
+        required: true
     },
-    UserPhone:{
-        type:String,
-        default:'none'
+    UserPhone: {
+        type: String,
+        default: 'none'
     },
-    UserPassword:{
-        type:String,
-        require:true
+    UserPassword: {
+        type: String,
+        required: true
     },
-    UserEmail:{
-        type:String,
-        require:true,
-        unique:true
+    UserEmail: {
+        type: String,
+        required: true,
+        unique: true
     },
-    UserRole:{
-        type:String,
-        enm:['user','vendor','admin'],
-        default:'user'
+    UserRole: {
+        type: String,
+        enum: ['user', 'vendor', 'admin'],
+        default: 'user'
     },
-    UserCreatedAt:{
-        type:Date,
-        default:Date.now()
+    UserCreatedAt: {
+        type: Date,
+        default: Date.now
     },
-    UserProfile:{
-        type:String,
-        default:'none'
+    UserProfile: {
+        type: String,
+        default: 'none'
     }
 });
 
-const userSchema = mongoose.model('UserSchema',UserSchema);
+const userSchema = mongoose.model('UserData', UserSchema);
 
-module.exports=userSchema;
+module.exports = userSchema;
